39f6f32453e08bbb9ea69d53ace48cc6fba6dcac
nabilla <nabilla@avana.id>
2020-11-14 00:35:32 +0700

    Initial Commit

---
+++ ThemeManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThemeManager
+//
+//  Created by nabilla on 14/11/20.
+//  Copyright © 2020 nabilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ThemeManagerTests/ThemeManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  ThemeManagerTests.swift
+//  ThemeManagerTests
+//
+//  Created by nabilla on 14/11/20.
+//  Copyright © 2020 nabilla. All rights reserved.
+//
+
+import XCTest
+@testable import ThemeManager
+
+class ThemeManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
